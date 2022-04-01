9602963bc727bb62ae7994423b79c321a339fe0d
Katayoun Timme <katayoun@timme.xyz>
2022-04-01 13:18:19 +0200

    Init next homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="pt-12 text-gray-100 flex-1 min-h-full h-full mx-auto max-w-screen-md px-4">
